85c5b749447a329690c9c1c6a71272f54efd903a
kamleshsharma-labs <kamlesh.sharma@greylabs.com>
2025-08-28 17:00:29 +0530

    Improve UI inteface of product card by using tailwaind css

--- frontend/src/app/products-public/page.tsx
+++ frontend/src/app/products-public/page.tsx
@@ -1,11 +1,9 @@
 'use client';
 import React, { useState, useEffect } from 'react';
-import ProductCard from '@/components/ProductCard';
 import { Product } from '@/types/product';
 
 const ProductsPublicPage: React.FC = () => {
   const [products, setProducts] = useState<Product[]>([]);
-  
   useEffect(() => {
     fetchProducts();
   }, []);
@@ -14,7 +12,7 @@ const ProductsPublicPage: React.FC = () => {
       const res = await fetch("http://localhost:3001/api/products");
       if (!res.ok) throw new Error("Failed to fetch products");
       const data = await res.json();
-      setProducts(data.data); 
+      setProducts(data.data);
     } catch (err) {
       console.error(err);
     }
@@ -29,12 +27,32 @@ const ProductsPublicPage: React.FC = () => {
         </div>
 
         {
-        products && 
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-          {products.map(product => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-        </div>
+          products &&
+          <div className="bg-white">
+            <div className="mx-auto max-w-2xl px-4 py-8 sm:px-6 sm:py-8 lg:max-w-7xl lg:px-8">
+              <div className="mt-6 grid grid-cols-1 gap-x-3 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+                {
+                  products.map(product => (
+                    <div className="group relative">
+                      <img src="https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="Front of men&#039;s Basic Tee in black." className="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80" />
+                      <div className="mt-4 flex justify-between">
+                        <div>
+                          <h3 className="text-sm text-gray-700">
+                            <a href="#">
+                              <span aria-hidden="true" className="absolute inset-0"></span>
+                              {product.name}
+                            </a>
+                          </h3>
+                          <p className="mt-1 text-sm text-gray-500">{product.description}</p>
+                        </div>
+                        <p className="text-sm font-medium text-gray-900">{product.price}</p>
+                      </div>
+                    </div>
+                  ))
+                }
+              </div>
+            </div>
+          </div>
         }
       </div>
     </div>
